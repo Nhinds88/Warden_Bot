c503ce32037438a17dd6f8f78e82382e5d013ccd
Nhinds88 <nhinds@csumb.edu>
2022-02-10 16:11:12 -0800

    command structure in place and working

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import json
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 
 client = commands.Bot(command_prefix="/")
-token = os.environ.get('DISCORD_BOT_SECRET')
+# token = os.environ.get('DISCORD_BOT_SECRET')
+token = 'OTQxNDUwODk3NTYzNzQ2MzI1.YgWIeQ.uKdUxmcCg4N85zdvYYQh9XibDDM';
 
 @client.event
 async def on_ready():
@@ -16,7 +17,7 @@ async def addguild(ctx, *, guildname):
     await ctx.send("Guild to be added to DB "+guildname);
     
 @client.command(pass_contect=True)
-async def editguild(ctx, *, oldguildname, newguildname):
+async def editguild(ctx, oldguildname, newguildname):
     await ctx.send("Edit Guild "+oldguildname+" to "+newguildname);
     
 @client.command(pass_contect=True)
@@ -24,21 +25,24 @@ async def removeguild(ctx, *, guildname):
     await ctx.send("remove guild from db "+guildname);
     
 @client.command(pass_contect=True)
-async def register(ctx, *, charName, guild, charClass, tier, mem):
+async def register(ctx, charName, guild, charClass, tier, mem):
     await ctx.send("register the member with the following: "+charName+" , "+guild+" , "+charClass+" , "+tier+" , "+mem);
     
 @client.command(pass_contect=True)
-async def update(ctx, *, charName, guild, charClass, tier, mem):
+async def update(ctx, charName, guild, charClass, tier, mem):
     await ctx.send("update the member with the following: "+charName+" , "+guild+" , "+charClass+" , "+tier+" , "+mem);
     
 @client.command(pass_contect=True)
-async def default(ctx, *, charName,  mem):
+async def default(ctx, charName,  mem):
     await ctx.send("set the follow character as default "+charName+" , "+mem);
     
 @client.command(pass_contect=True)
-async def remove(ctx, *, charName, mem):
+async def remove(ctx, charName, mem):
     await ctx.send("remove the following character: "+charName+" , "+mem);
     
 @client.command(pass_contect=True)
 async def whois(ctx, *, charName):
     await ctx.send("returns the owner of : "+charName);
+    
+    
+client.run(token)
