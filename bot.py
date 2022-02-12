7fde7832b34fe3a95638308ac6704085c72848cb
Nhinds88 <nhinds@csumb.edu>
2022-02-11 16:04:18 -0800

    classes and tiers

--- bot.py
+++ bot.py
@@ -2,11 +2,14 @@ import json
 import discord
 from discord.ext import commands
 # from dotenv import load_dotenv
+from replit import db
 import os
+
+# https://www.freecodecamp.org/news/create-a-discord-bot-with-python/
 
 client = commands.Bot(command_prefix="/")
 # token = os.environ.get('DISCORD_BOT_SECRET')
-token = 'OTQxNDUwODk3NTYzNzQ2MzI1.YgWIeQ.uKdUxmcCg4N85zdvYYQh9XibDDM';
+token = 'xxx';
 
 @client.event
 async def on_ready():
@@ -43,6 +46,21 @@ async def remove(ctx, charName, mem):
 @client.command(pass_contect=True)
 async def whois(ctx, *, charName):
     await ctx.send("returns the owner of : "+charName);
-    
+
+@client.command(pass_contect=True)
+async def classes(ctx):
+    classes = ['Berserker', 'Gunlancer', 'Paladin', 'Destroyer', 'Deadeye', 'Sharpshooter', 'Artillerist', 'Scouter', 'Gunslinger', 'Wardancer', 'Scrapper', 'Striker', 'Soulfist', 'Lance Master', 'Summoner', 'Bard', 'Arcana', 'Sorceress', 'Deathblade'];
+    await ctx.send("Class List: ");
+
+    for x in range(len(classes)):
+      await ctx.send(classes[x]);
+
+@client.command(pass_contect=True)
+async def tiers(ctx):
+    tier_list = ['N/A', 'T1', 'T2', 'T3']
+    await ctx.send("Tier List: ");
+
+    for x in range(len(tier_list)):
+      await ctx.send(tier_list[x]);
     
 client.run(token)
