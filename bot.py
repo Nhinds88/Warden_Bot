3669718ada6503576f39d4e960160ecb2a0c2407
Nhinds88 <nhinds@csumb.edu>
2022-02-14 15:44:08 -0800

    code for commands in place

--- bot.py
+++ bot.py
@@ -13,39 +13,75 @@ token = 'xxx';
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client));
     
 @client.command(pass_contect=True)
-async def addguild(ctx, *, guildname):
-    await ctx.send("Guild to be added to DB "+guildname);
+async def addguild(ctx, guildname):
+    if guildname in db.keys():
+      await ctx.send("This guild has already been added")
+    else:
+        db[guildname] = guildname;
+        await ctx.send(guildname+" Has been added!");
     
 @client.command(pass_contect=True)
 async def editguild(ctx, oldguildname, newguildname):
-    await ctx.send("Edit Guild "+oldguildname+" to "+newguildname);
+    if oldguildname in db.keys():
+      del db[oldguildname];
+      db[newguildname] = newguildname;
+      await ctx.send("Guild "+oldguildname+" has been edited to "+newguildname);
+    else:
+      await ctx.send("No Guild named "+oldguildname+" found!");
     
 @client.command(pass_contect=True)
 async def removeguild(ctx, *, guildname):
-    await ctx.send("remove guild from db "+guildname);
+    if guildname in db.keys():
+      del db[guildname];
+      await ctx.send(guildname+" has been deleted!");
+    else:
+      await ctx.send("No guild name "+guildname+" found!");
     
 @client.command(pass_contect=True)
 async def register(ctx, charName, guild, charClass, tier, mem):
-    await ctx.send("register the member with the following: "+charName+" , "+guild+" , "+charClass+" , "+tier+" , "+mem);
+    if charName in db.keys():
+      await ctx.send("your character has already been added.");
+    else:
+      db[charName] = [charName, guild, charClass, tier, mem];
+      await ctx.send(charName+" has been addded!");
     
 @client.command(pass_contect=True)
-async def update(ctx, charName, guild, charClass, tier, mem):
-    await ctx.send("update the member with the following: "+charName+" , "+guild+" , "+charClass+" , "+tier+" , "+mem);
+async def update(ctx, oldCharName, charName, guild, charClass, tier, mem):
+    if oldCharName in db.keys():
+      del db[oldCharName];
+      db[charName] = [charName, guild, charClass, tier, mem];
+      await ctx.send(charName+" has been updated!");
+    else:
+      await ctx.send("No character "+charName+" found.");
     
 @client.command(pass_contect=True)
 async def default(ctx, charName,  mem):
-    await ctx.send("set the follow character as default "+charName+" , "+mem);
+    if charName in db.keys():
+      db[mem] = [charName, mem, "default"];
+      await ctx.send("Your default is set, "+mem);
+    else:
+      await ctx.send(charName+" not found, please register your character first.");
     
 @client.command(pass_contect=True)
 async def remove(ctx, charName, mem):
-    await ctx.send("remove the following character: "+charName+" , "+mem);
+    if charName in db.keys():
+      del db[charName];
+      if mem in db.keys():
+        del db[mem];
+      await ctx.send(charName+" deleted!");
+    else:
+      await ctx.send(charName+" Not Found!");
     
 @client.command(pass_contect=True)
 async def whois(ctx, *, charName):
-    await ctx.send("returns the owner of : "+charName);
+    if charName in db.keys():
+      charProf = db[charName]
+      await ctx.send(charName+" is "+charProf[4]);
+    else:
+      await ctx.send(charName+" Not found!");
 
 @client.command(pass_contect=True)
 async def classes(ctx):
